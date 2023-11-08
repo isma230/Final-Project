0d8d7431ce67e2990937b285b5cbc97677bc4049
isma230 <esmailchari123@gmail.com>
2023-11-08 16:21:50 +0100

    dashboard done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import Dashboard from './back-office/dashboardPage';
 function App() {
 
   return (
-    <>
- 
-    </>
+    <div>
+      <Dashboard />
+    </div>
   )
 }
 
-export default App
+export default App;
